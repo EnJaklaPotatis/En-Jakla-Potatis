8721c7290c9ae67b3a8275923a201807e0944539
EnJaklaPotatis <134811667+EnJaklaPotatis@users.noreply.github.com>
2024-06-04 09:40:26 +0200

    Script added for buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("en").onclick = English();
+document.getElementById("se").onclick = Svenska();
+
+function Svenska(){
+console.log("SE");
+}
+function English(){
+console.log("EN");
+}
