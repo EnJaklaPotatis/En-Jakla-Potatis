a2cc99589d23a16822eec6fc87d90babf1156141
elias linde <ell438972@gmail.com>
2025-04-24 20:52:35 +0200

    Changing stuff

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ document.getElementById("en").onclick = English;
 document.getElementById("se").onclick = Svenska;
 //f
 function Svenska(){
-console.log("SE");
+console.log("SV");
 }
 function English(){
 console.log("EN");
