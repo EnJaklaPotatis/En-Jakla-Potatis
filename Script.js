9d9f429c0ddea2a23dfae7044dbc83cd7e0699ad
elias linde <ell438972@gmail.com>
2025-04-24 22:01:47 +0200

    Fixed a misstake.

--- Script.js
+++ Script.js
@@ -4,7 +4,7 @@ const lang = params.get("lang") || "en";
 fetch(`/lang/${lang}.json`)
     .then(res => res.json())
     .then(data => {
-        document.getElementById("Language").textContent = data.title;
+        document.getElementById("Language").textContent = data.Language;
     })
     .catch(() => {
         document.getElementById("Language").textContent = "Translation error";
